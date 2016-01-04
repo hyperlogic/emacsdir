cd600c51a93330726500dcd0843b40e2c73bce4b
Anthony J. Thibault <tony@highfidelity.io>
2016-01-04 13:07:35 -0800

    syntax fix

--- lisp/ajt-windows-init.el
+++ lisp/ajt-windows-init.el
@@ -3,5 +3,5 @@
 
 (if (equal hostname "blackholesun")
     (set-face-attribute 'default nil :family "Hack" :height 90)
-  (set-face-attribute 'default nil :family "Hack" :height 90))
+  (set-face-attribute 'default nil :family "Hack" :height 85))
 
