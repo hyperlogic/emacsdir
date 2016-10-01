5591e32e61f757d9abc641c81bb4d23174436351
Anthony Thibault <ajt@hyperlogic.org>
2016-10-01 11:36:25 -0700

    home

--- lisp/ajt-windows-init.el
+++ lisp/ajt-windows-init.el
@@ -3,6 +3,6 @@
 
 (if (equal hostname "blackholesun")
     (set-face-attribute 'default nil :family "Consolas" :height 110 :weight 'regular)
-  (set-face-attribute 'default nil :family "Hack" :height 90))
+  (set-face-attribute 'default nil :family "Hack" :height 110))
 
 
