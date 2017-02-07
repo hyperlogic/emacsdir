47a24f80556d122926d5e46b5becfc984ecf4153
Anthony J. Thibault <tony@highfidelity.io>
2017-02-07 11:14:38 -0800

    git changes

--- lisp/ajt-git.el
+++ lisp/ajt-git.el
@@ -9,11 +9,21 @@
     (goto-line line)))
 
 ;; invoke git log on current buffer.
+;; alias gl="git --no-pager log -15 --graph --pretty=\"tformat:%C(green)%h %C(red)%an %C(white)%s %C(green)%cr%C(reset)\" --abbrev-commit"
 (defun ajt-git-log ()
   (interactive)
   (let ((line (line-number-at-pos))
         (file-name (file-name-nondirectory (buffer-file-name))))
-    (shell-command (concat "git log \"" file-name "\"") "*ajt-git-log*")
+    (shell-command (concat "git --no-pager log -100 --graph --pretty=\"tformat:%h %an %s %cr\" --abbrev-commit \"" file-name "\"") "*ajt-git-log*")
+    (pop-to-buffer "*ajt-git-log*")))
+
+
+;; invoke git log on current buffer.
+(defun ajt-git-log-full ()
+  (interactive)
+  (let ((line (line-number-at-pos))
+        (file-name (file-name-nondirectory (buffer-file-name))))
+    (shell-command (concat "git log -p \"" file-name "\"") "*ajt-git-log*")
     (pop-to-buffer "*ajt-git-log*")))
 
 ;; invoke git diff and pipe result into a buffer.
