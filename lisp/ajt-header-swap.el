147a31ea73195954f91ff5101d738912ad4a8294
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2020-04-14 11:17:55 -0700

    ajt-header-swap: Now handles header and cpp files in different directories
    Such as ../Public/ or ../Private/ which is the convention of the UnrealEngine.

--- lisp/ajt-header-swap.el
+++ lisp/ajt-header-swap.el
@@ -1,6 +1,8 @@
 ;; list of header and soruce file extentions
-(setq ajt-hdr-ext-list `(".h" ".hpp" ".vsh" ".hh" ".slv"))
-(setq ajt-src-ext-list `(".cpp" ".c" ".m" ".mm" ".fsh" ".cc" ".slf"))
+(setq ajt-hdr-path-list `("./" "../Public/"))
+(setq ajt-hdr-ext-list `("h" "hpp" "vsh" "hh" "slv"))
+(setq ajt-src-path-list `("./" "../Private/"))
+(setq ajt-src-ext-list `("cpp" "c" "m" "mm" "fsh" "cc" "slf"))
 
 ;; fn can return non-nil to stop iteration
 (defun ajt-for-each (fn lst)
@@ -10,31 +12,29 @@
         't
       (ajt-for-each fn (cdr lst)))))
 
-;; if file exists open it and return 't, otherwise return nil
-(defun ajt-find-file-with-ext (basename ext)
-  (let ((full-filename (concat basename ext)))
-    (if (file-exists-p full-filename)
-        (progn (find-file full-filename) 't)
-      nil)))
+;; flatten a list of lists into a single list
+;; for example (ajt-flatten '((a b) (c d))) => (a b c d)
+(defun ajt-flatten (list-of-lists)
+  (apply #'append list-of-lists))
+
+;; build list of expanded filenames to search for.
+(defun ajt-build-filenames ()
+  (let* ((filename (buffer-file-name (current-buffer)))
+         (ext (file-name-extension filename))
+         (dir (file-name-directory filename))
+         (base (file-name-base filename)))
+    (if (member ext ajt-hdr-ext-list)
+        (ajt-flatten (mapcar (lambda (p) (mapcar (lambda (e) (concat dir p base "." e)) ajt-src-ext-list)) ajt-src-path-list))
+      (ajt-flatten (mapcar (lambda (p) (mapcar (lambda (e) (concat dir p base "." e)) ajt-hdr-ext-list)) ajt-hdr-path-list)))))
 
 ;; Swap between .h/.hpp and .cpp/.c/.m/.mm files
 (defun ajt-header-swap ()
   "Swap between .h & .cpp files"
   (interactive)
-  (let* ((filename (buffer-file-name (current-buffer)))
-         (ext (file-name-extension filename))
-         (basename (file-name-sans-extension filename)))
-
-    ;; if current-buffer is a header
-    (if (member (concat "." ext) ajt-hdr-ext-list)
-        ;; try to open corresponding src file
-        (if (ajt-for-each (lambda (ext) (ajt-find-file-with-ext basename ext)) ajt-src-ext-list)
-            't
-          (error "could not find corresponding src file for \"%s\"" filename))
-      ;; try to open corresponding header file
-      (if (ajt-for-each (lambda (ext) (ajt-find-file-with-ext basename ext)) ajt-hdr-ext-list)
-          't
-        (error "could not find header file for \"%s\"" filename)))))
+  (let ((filenames (ajt-build-filenames)))
+    (if (ajt-for-each (lambda (f) (if (file-exists-p f) (progn (find-file f) 't) nil)) filenames)
+        't
+      (error "could not find corresponding file for \"%s\"" (buffer-filename (current-buffer))))))
 
 ;; was mark-whole-buffer
 (global-set-key "\C-x\h" 'ajt-header-swap)
