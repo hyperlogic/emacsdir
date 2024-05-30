1741f1ef9b340ce0322c3222b93cc7ef5a9c418c
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-05-30 09:09:55 -0700

    udpate from windows

--- lisp/ajt-windows-init.el
+++ lisp/ajt-windows-init.el
@@ -26,7 +26,7 @@
 ;(set-face-attribute 'default nil :family "Glass TTY VT220" :height 120 :weight 'regular)
 
 
-(set-face-attribute 'default nil :family "Consolas" :height 140 :weight 'regular)
+(set-face-attribute 'default nil :family "Consolas" :height 160 :weight 'regular)
 ;(set-face-attribute 'default nil :family "Ac437 IBM EGA 8x14" :height 160 :weight 'regular)
 
 
