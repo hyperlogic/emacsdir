0a9c7afe2454375d58d173fd45a1f9ce601a5b49
Tony Thibault <t@uthana.com>
2024-11-18 21:08:04 +0000

    dont use doom theme on command line

--- init.el
+++ init.el
@@ -154,23 +154,25 @@
 ;;
 (load "ajt-indent")
 
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one-light t)
+(if (window-system)
+    (progn
+      (use-package doom-themes
+                   :ensure t
+                   :config
+                   ;; Global settings (defaults)
+                   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+                         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+                   (load-theme 'doom-one-light t)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+                   ;; Enable flashing mode-line on errors
+                   (doom-themes-visual-bell-config)
+                   ;; Enable custom neotree theme (all-the-icons must be installed!)
+                   (doom-themes-neotree-config)
+                   ;; or for treemacs users
+                   (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+                   (doom-themes-treemacs-config)
+                   ;; Corrects (and improves) org-mode's native fontification.
+                   (doom-themes-org-config))))
 
 ;;
 ;; color theme
