25354bf5edae8b8f333cdb0f23c17d7e7c36b229
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-12-08 08:35:21 -0800

    save-buffer before black

--- lisp/ajt-py-helpers.el
+++ lisp/ajt-py-helpers.el
@@ -1,6 +1,7 @@
 ;; black
 (defun ajt-black ()
   (interactive)
+  (save-buffer)
   (shell-command (concat "black --line-length=120 " (buffer-file-name)) "*black-log*")
   (pop-to-buffer "*black-log*")
   (compilation-mode))
@@ -8,6 +9,7 @@
 ;; flake8
 (defun ajt-flake8 ()
   (interactive)
+  (save-buffer)
   (shell-command (concat "flake8 " (buffer-file-name)) "*flake8-log*")
   (pop-to-buffer "*flake8-log*")
   (compilation-mode))
@@ -15,6 +17,7 @@
 ;; isort
 (defun ajt-isort ()
   (interactive)
+  (save-buffer)
   (shell-command (concat "isort --profile=black --line-length=120 " (buffer-file-name)) "*flake8-log*")
   (pop-to-buffer "*isort-log*")
   (compilation-mode))
@@ -22,6 +25,7 @@
 ;; isort
 (defun ajt-mypy ()
   (interactive)
+  (save-buffer)
   (shell-command (concat "mypy " (buffer-file-name)) "*mypy-log*")
   (pop-to-buffer "*mypy-log*")
   (compilation-mode))
