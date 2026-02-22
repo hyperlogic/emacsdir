58ec59303710b8d18187313a53fb10fde0724301
Anthony Thibault <ajt.hyperlogic@gmail.com>
2026-02-22 08:38:51 -0800

    update from mac

--- lisp/ajt-macintosh-init.el
+++ lisp/ajt-macintosh-init.el
@@ -7,8 +7,12 @@
 ;; use aspell
 (setq-default ispell-program-name "/opt/homebrew/bin/aspell")
 
+;; need this to make the backspace key work in eat.
+(with-eval-after-load 'eat
+  (setq eat-term-name "xterm-256color"))
+
 ;(set-face-attribute 'default nil :family "Menlo" :height 140)
-(set-face-attribute 'default nil :family "JetBrains Mono NL" :height 140 :weight 'light)
+(set-face-attribute 'default nil :family "JetBrains Mono NL" :height 140 :weight 'medium)
 ;;(set-face-attribute 'default nil :family "Hack" :height 130)
 ;;(set-face-attribute 'default nil :family "Glass TTY VT220" :height 130)
 
